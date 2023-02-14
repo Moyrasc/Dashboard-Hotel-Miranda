60c9695b1a42b9304cdb590933bd488fcabf6144
Moyrasc <noemisolac@gmail.com>
2023-02-14 10:06:20 +0100

    #2: add links routes in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+    <nav>
+    <Link to={"/"}><h4>Dashboard</h4></Link>
+    <Link to={"bookings"}><h4>Booking</h4></Link>
+    <Link to={"rooms"}><h4>Rooms</h4></Link>
+    <Link to={"contacts"}><h4>Contact</h4></Link>
+    <Link to={"users"}><h4>Users</h4></Link>
+    </nav>
+    </>
   )
 }
 
