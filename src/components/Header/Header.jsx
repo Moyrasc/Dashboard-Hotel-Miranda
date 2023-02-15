d6568f256e542eb1de4de45db33e5efa02a56d0b
Moyrasc <noemisolac@gmail.com>
2023-02-15 14:19:34 +0100

    #15: add dinamic title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   HeaderTitle,
   HeaderContainer,
@@ -6,13 +6,42 @@ import {
 } from "./HeaderStyled";
 import {  AiOutlineBell, AiOutlineMail } from "react-icons/ai";
 import {FiLogOut} from 'react-icons/fi'
+import { useLocation } from "react-router";
 
 
 const Header = () => {
+
+  let location = useLocation()
+  const [title, setTitle] = useState("")
+
+  useEffect(()=>{
+    switch(location.pathname){
+      
+      case "/":
+        setTitle("Dashboard");
+        break
+      case "/bookings/list":
+        setTitle("Bookings");
+                break;
+            case "/rooms/list":
+                setTitle("Rooms");
+                break;
+            case "/contacts/list":
+                setTitle("Contacts");
+                break;
+            case "/users/list":
+                setTitle("Users");
+                break;
+            default:
+              setTitle("")  
+
+    }
+  },[location])
+  
   return (
     <HeaderContainer>
       <HeaderTitle>
-        <h1> Dashboard</h1>
+        <h1> {title}</h1>
       </HeaderTitle>
         <Icon>
           
