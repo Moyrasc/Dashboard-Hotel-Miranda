119829a983c6fa0326bb11a0a98d8b862bacdb70
Moyrasc <noemisolac@gmail.com>
2023-02-17 15:41:34 +0100

    change basename

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <UserProvider>
-      <BrowserRouter basename='/'>
+      <BrowserRouter basename='/Dashboard-Hotel-Miranda'>
         <App />
       </BrowserRouter>
     </UserProvider >
