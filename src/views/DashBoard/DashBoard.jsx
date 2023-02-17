c9a54828edbd98fb9a3af511602e9c296e482fe2
Moyrasc <noemisolac@gmail.com>
2023-02-17 21:40:46 +0100

    start dashboard desing

--- src/views/DashBoard/DashBoard.jsx
+++ src/views/DashBoard/DashBoard.jsx
@@ -1,12 +1,56 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { DashboardContainer, IconsDashboard, KpiContainer, KpiNumbers, KpiParagraph } from "./DashboardStyled";
+import { MdOutlineBed } from 'react-icons/md'
 
 const DashBoard = () => {
 
-    return(
-        <>
-        <Outlet/>
-        </>
+    return (
+        <DashboardContainer>
+            <KpiContainer>
+                <div className="iconBed">
+                <IconsDashboard>
+                    <MdOutlineBed />
+                </IconsDashboard>
+                </div>
+                <div>
+                <KpiNumbers>8,461</KpiNumbers>
+                <KpiParagraph>New booking</KpiParagraph>
+                </div>
+            </KpiContainer>
+            <KpiContainer>
+                <div className="iconBed">
+                <IconsDashboard>
+                    <MdOutlineBed />
+                </IconsDashboard>
+                </div>
+                <div>
+                <KpiNumbers>963</KpiNumbers>
+                <KpiParagraph>Scheduled Room</KpiParagraph>
+                </div>
+            </KpiContainer>
+            <KpiContainer>
+                <div className="iconBed">
+                <IconsDashboard>
+                    <MdOutlineBed />
+                </IconsDashboard>
+                </div>
+                <div>
+                <KpiNumbers>753</KpiNumbers>
+                <KpiParagraph>Check In</KpiParagraph>
+                </div>
+            </KpiContainer>
+            <KpiContainer>
+                <div className="iconBed">
+                <IconsDashboard>
+                    <MdOutlineBed />
+                </IconsDashboard>
+                </div>
+                <div>
+                <KpiNumbers>516</KpiNumbers>
+                <KpiParagraph>Check Out</KpiParagraph>
+                </div>
+            </KpiContainer>
+        </DashboardContainer>
     )
 }
 
---
+++ src/views/DashBoard/DashboardStyled.jsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+
+const DashboardContainer = styled.div`
+display: flex;
+`;
+const IconsDashboard = styled.div`
+background-color: #FFEDEC;
+color: #E23428;
+width:65px;
+height: 65px;
+border-radius: 8px;
+display: flex;
+justify-content: center;
+align-items: center;
+
+`
+const KpiContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+    background-color: #FFFFFF;
+    border-radius: 12px;
+    width: 250px;
+    margin-top: 1rem;
+    justify-content: flex-start;
+    margin-left: 10px;
+    .iconBed {
+    display: flex;
+    padding: 0 16px;
+    align-items: center;
+}
+
+`;
+const KpiNumbers = styled.p`
+    font-size: 24px;
+    font-family: var(--font-poppins);
+    font-weight: 600;
+    color: #393939;
+    opacity: 1;
+    margin-bottom: 0;
+`;
+
+const KpiParagraph = styled.p`
+    font-family: var(--font-poppins);
+    font-size: 14px;
+    font-weight: 300;
+    color: #787878;
+    margin-top: 0;
+`
+
+
+export{
+    DashboardContainer,
+    IconsDashboard,
+    KpiContainer,
+    KpiNumbers,
+    KpiParagraph
+}
