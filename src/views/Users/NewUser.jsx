483601882fef717b2c23053a3f87f4ef48b954a5
Moyrasc <noemisolac@gmail.com>
2023-02-18 14:58:48 +0100

    created new user form

--- src/views/Users/NewUser.jsx
+++ src/views/Users/NewUser.jsx
@@ -1,9 +1,42 @@
 import React from "react";
+import { Input, InputContainer } from "../Login/LoginStyled";
+import { FormContainer, SelectUser, UserContainer } from "./UsersStyled";
 
 const NewUser = () => {
 
     return(
-        <h1>Desde crear Usuario</h1>
+        <UserContainer>
+            <FormContainer>
+                <InputContainer>
+                <form enctype="multipart/formdata/" name="upload-image">
+                    <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="avatar"/>
+                </form>
+                
+                <label>Full Name</label>
+                <Input type="text" />
+                <label>Job Position</label>
+                <SelectUser name="Job Desk">
+                    <option value="Manager">Manager</option>
+                    <option value="Reception" selected>Reception</option>
+                    <option value="RoomService">Room Service</option>
+                </SelectUser>
+                <label>Email</label>
+                <Input type="email"/>
+                <label>Password</label>
+                <Input type="password" />
+                <label>Phone Number</label>
+                <Input type="tel" pattern="\([0-9]{3}\) [0-9]{3}[ -][0-9]{4}"/>
+                <label>Start Date</label>
+                <Input type="date"/>
+                <label>Job Description</label>
+                <textarea name="textarea" rows="5" cols="30">Write job description here...</textarea>
+                </InputContainer>
+                <div className="btn-save">
+                    <button>Save User</button>
+                </div>
+            </FormContainer>
+            
+        </UserContainer>
     )
 }
 
