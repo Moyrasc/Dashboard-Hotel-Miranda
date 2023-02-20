7ddc9613e8c6f387a5987932c6e55e5726fb22ec
Moyrasc <noemisolac@gmail.com>
2023-02-20 15:08:40 +0100

    :#25 create bookingslice

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Navigate, Outlet } from "react-router";
 import Header from "./Header/Header";
 import Navbar from "./Navbar/Navbar"
@@ -7,11 +7,9 @@ import { useUser } from "../Context/userContext";
 
 const ProtectedRoute = () => {
     const {user} = useUser()
-    
         if (user.logged === false) {
         return <Navigate to='/login' replace />
-    } 
-   
+    }  
         return (
             <Layout>  
                 <Header  />
---
+++ src/features/slices/bookingsSlice.js
@@ -0,0 +1,109 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import delay from './delay.js'
+import bookings from '../../Data/bookings.json'
+
+export const fetchAllBookings = createAsyncThunk(
+    "bookings/fetchBookings",
+    async () => {
+        return await delay(bookings)
+    }
+)
+export const fetchBooking = createAsyncThunk(
+    "booking/fetchBooking",
+    async (id) => {
+        const singleBooking = bookings.find((element) => element.id === id)
+        const booking = await delay(singleBooking)
+        return booking
+    }
+
+)
+export const newBooking = createAsyncThunk(
+    "bookings/newBooking",
+    async () => {
+        return await delay(newBooking)
+    }
+)
+export const editBooking = createAsyncThunk(
+    "booking/editBooking",
+    async (id) => {
+        const upgradeBooking = bookings.find((element) => element.id === id)
+        const booking = await delay(upgradeBooking)
+        return booking
+    }
+)
+export const deleteBooking = createAsyncThunk(
+    "booking/deleteBooking",
+    async (id) => {
+        const delBooking = bookings.find((element) => element.id === id)
+        const booking = await delay(delBooking)
+        return booking
+    }
+
+)
+const initialState = {
+    bookings: [],
+    booking: {},
+    status: 'loading'
+
+}
+export const bookingsSlice = createSlice({
+    name: 'bookings',
+    initialState,
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchAllBookings.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchAllBookings.fulfilled, (state, action) => {
+                state.status = 'fulfilled'
+                state.results = action.payload
+            })
+            .addCase(fetchAllBookings.rejected, (state) => {
+                state.status = 'error'
+            })
+            .addCase(fetchBooking.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(fetchBooking.fulfilled, (state, action) => {
+                state.status = 'fulfilled'
+                state.results = action.payload
+            })
+            .addCase(fetchBooking.rejected, (state) => {
+                state.status = 'error'
+            })
+            .addCase(newBooking.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(newBooking.fulfilled, (state, action) => {
+                state.status = 'fulfilled'
+                state.results = action.payload
+            })
+            .addCase(newBooking.rejected, (state) => {
+                state.status = 'error'
+            })
+            .addCase(editBooking.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(editBooking.fulfilled, (state, action) => {
+                state.status = 'fulfilled'
+                state.results = action.payload
+            })
+            .addCase(editBooking.rejected, (state) => {
+                state.status = 'error'
+            })
+            .addCase(deleteBooking.pending, (state) => {
+                state.status = 'loading'
+            })
+            .addCase(deleteBooking.fulfilled, (state, action) => {
+                state.status = 'fulfilled'
+                state.results = action.payload
+            })
+            .addCase(deleteBooking, (state) => {
+                state.status = 'error'
+            })
+    }
+})
+export const selectAlltBooking = (state) => state.bookings.bookings;
+export const selectBooking = (state) => state.bookings.booking;
+
+export default bookingsSlice.reducer
