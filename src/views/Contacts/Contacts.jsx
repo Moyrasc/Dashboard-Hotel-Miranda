ad329a5c16e9435a477c2e890924b2a46ae86656
Moyrasc <noemisolac@gmail.com>
2023-02-21 21:19:47 +0100

    :#26 working contacts page pagination and connecting slice

--- src/views/Contacts/Contacts.jsx
+++ src/views/Contacts/Contacts.jsx
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 // import { Outlet } from "react-router";
 import Table from "../../components/Table/Table";
 import contacts from "../../Data/contacts.json";
 import { ActionContainer, Archive, Public } from "./ContactsStyled";
-import { FilterButton, FilterTable, ButtonContainer, SelectOrder } from "../../components/Table/TableStyled";
-import {IoIosArrowDown} from 'react-icons/io'
+import { FilterTable, ButtonContainer, SelectOrder } from "../../components/Table/TableStyled";
+import { IoIosArrowDown } from 'react-icons/io'
+import Switch from "../../components/Switch/Switch";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllContacts, selectAlltContacts } from "../../features/slices/contactsSlice";
+import Pagination from "../../components/Pagination/Pagination";
 
 const Contacts = () => {
+  const contacts = useSelector(selectAlltContacts)
+  const dispatch = useDispatch()
+  const [contactsState, setContactsState] = useState([])
+  const [filter, setFilter] = useState('')
+  const [orderBy, setOrderBy] = useState('id')
+  const [currentPage, setCurrentPage] = useState(1);
+  const roomsPerPage = 5;
+  const indexOfLastItem = currentPage * roomsPerPage;
+  const indexOfFirstItem = indexOfLastItem - roomsPerPage;
+
+  useEffect(() => {
+    dispatch(fetchAllContacts())
+  }, [dispatch])
+  useEffect(() => {
+    const orderFilterContacts = contacts.filter(contact => contact.customer.toLowerCase())
+    orderFilterContacts.sort((a, b) => {
+      if (a[orderBy] > b[orderBy]) {
+        return 1;
+      } else if (a[orderBy] < b[orderBy]) {
+        return -1;
+      }
+      return 0;
+    })
+    setContactsState(orderFilterContacts)
+  }, [contacts, orderBy])
+  const filterContacts = useMemo(() => {
+    if (filter === '') return contactsState
+    const filteredsContacts = contactsState.filter(contact => contact.customer === filter)
+    return filteredsContacts
+  }, [filter, contactsState])
+
+  const contactsPagination = useMemo(() => {
+    return filterContacts.slice(indexOfFirstItem, indexOfLastItem)
+  }, [filterContacts, indexOfFirstItem, indexOfLastItem])
+  const handleFilter = (filter) => {
+    setFilter(filter)
+  }
+  const nPages = Math.ceil(filterContacts.length / roomsPerPage);
   const cols = [
 
     { property: 'id', label: 'Order ID' },
@@ -24,14 +66,17 @@ const Contacts = () => {
 
     <div>
       <FilterTable>
-                    <FilterButton >All Customer Reviews</FilterButton>
-                    <FilterButton>Published</FilterButton>
-                    <FilterButton>Archived</FilterButton>
-                    <ButtonContainer>
-                        <SelectOrder>Newest <span><IoIosArrowDown/></span></SelectOrder>
-                    </ButtonContainer>
-                </FilterTable>
-      <Table data={contacts} cols={cols} />
+        <Switch items={[{ label: 'All Customer Reviews', value: '' }, { label: 'Published', value: 'actionPublish' },
+        { label: 'Archived', value: 'actionArchived' }]} handleSwitcher={handleFilter} />
+      </FilterTable>
+      <Table data={contactsPagination} cols={cols} />
+      <Pagination nPages={nPages}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                dataDisplayed={"rooms"}
+                totalItems={filterContacts.length}
+                indexOfFirstItem={indexOfFirstItem}
+                indexOfLastItem={indexOfLastItem} />
     </div>
   );
 };
