91cc015db195660f91942f6c5bbc6391726b7e08
Moyrasc <noemisolac@gmail.com>
2023-03-01 20:06:20 +0100

    completed test cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,19 +4,21 @@
 /* global cy */
 
 //LOGIN
+
 describe('Login', () => {
   it('visit login for first time and throws error if no data', () => {
-    cy.visit('http://localhost:3000/Dashboard-Hotel-Miranda')
+    cy.visit('http://localhost:3000/Dashboard-Hotel-Miranda/login')
     cy.get('[data-cy="login"]').click()
   })
   it('throws error wrong email or password', () => {
-    cy.visit('http://localhost:3000/Dashboard-Hotel-Miranda')
+    cy.visit('http://localhost:3000/Dashboard-Hotel-Miranda/login')
     cy.get('[data-cy="email"]').type('email@email.com')
     cy.get('[data-cy="password"]').type('12345')
     cy.get('[data-cy="login"]').click()
   })
+
   it('user login', () => {
-    cy.visit('http://localhost:3000/Dashboard-Hotel-Miranda')
+    cy.visit('http://localhost:3000/Dashboard-Hotel-Miranda/login')
     cy.get('[data-cy="email"]').type('admin@admin.com')
     cy.get('[data-cy="password"]').type('12345')
     // cy.get('[data-cy="login"]').click().should(() => {
@@ -25,13 +27,16 @@ describe('Login', () => {
     cy.get('[data-cy="login"]').click()
     cy.url().should('contain', '/')
   })
+
   it('user logout', () => {
-    // cy.visit('http://localhost:3000/Dashboard-Hotel-Miranda/dashboard')
-    // cy.get('[data-cy="logout"]').click().should(() => {
-    //   expect(JSON.parse(localStorage.getItem('userMiranda'))).to.deep.equal({ username: '', email: '', logged: false });
-    // })
-    cy.window().its('localStorage').invoke('removeItem', 'session')
     cy.visit('http://localhost:3000/Dashboard-Hotel-Miranda/login')
+    cy.get('[data-cy="email"]').type('admin@admin.com')
+    cy.get('[data-cy="password"]').type('12345')
+    cy.get('[data-cy="login"]').click()
+    cy.url().should('contain', '/')
+    cy.get('[data-cy="logout"]').click()
+    cy.window().its('localStorage').invoke('removeItem', 'session')
+    cy.url().should('include', 'http://localhost:3000/Dashboard-Hotel-Miranda/login')
   })
 })
 
